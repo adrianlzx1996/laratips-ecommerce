d1f4c7da6c4e15e1824fdc43dbfe74d6100bde23
Adrian Leong <leongadrian36@gmail.com>
2022-08-02 23:38:43 +0800

    new basic components

---
+++ resources/js/admin/Components/Card/Card.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg border-b border-gray-200">
+        <div class="p-6">
+            <slot/>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Card"
+}
+</script>
+
+<style scoped>
+
+</style>

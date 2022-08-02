c76dc0288790208e21e7054215bd21da01bb0e13
Adrian Leong <leongadrian36@gmail.com>
2022-08-02 23:01:43 +0800

    roles crud

--- routes/admin.php
+++ routes/admin.php
@@ -1,10 +1,12 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\DashboardController;
+    use App\Http\Controllers\Admin\DashboardController;
+    use App\Http\Controllers\Admin\RolesController;
+    use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth'])->group(function () {
-    Route::get('dashboard', DashboardController::class)->name("dashboard");
-});
+    Route::middleware([ 'auth' ])->group(function () {
+        Route::get('dashboard', DashboardController::class)->name("dashboard");
+        Route::resource('roles', RolesController::class);
+    });
 
-require __DIR__ . '/auth.php';
+    require __DIR__ . '/auth.php';

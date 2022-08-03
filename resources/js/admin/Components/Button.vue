69092f5fd62cb1c5e5b478e1bc6ade9f98a71cb8
Adrian Leong <leongadrian36@gmail.com>
2022-08-03 22:03:56 +0800

    basic components

--- resources/js/admin/Components/Button.vue
+++ resources/js/admin/Components/Button.vue
@@ -1,14 +1,26 @@
 <script setup>
+import { Link } from "@inertiajs/inertia-vue3";
+
 defineProps({
     type: {
         type: String,
         default: 'submit',
     },
+    href: {
+        type: String,
+        default: '',
+    },
 });
+
+const classes = "inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150";
 </script>
 
 <template>
-    <button :type="type" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150">
-        <slot />
+    <Link v-if="href" :class="classes" :href="href">
+        <slot/>
+    </Link>
+    <button v-else :class="classes"
+            :type="type">
+        <slot/>
     </button>
 </template>

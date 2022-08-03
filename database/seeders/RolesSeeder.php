84cec0fe0a615cd9b14d27573dbe6b8708399145
Adrian Leong <leongadrian36@gmail.com>
2022-08-03 11:07:55 +0800

    database seeder

---
+++ database/seeders/RolesSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+    namespace Database\Seeders;
+
+    use Illuminate\Database\Seeder;
+    use Spatie\Permission\Models\Role;
+
+    class RolesSeeder extends Seeder
+    {
+        /**
+         * Run the database seeds.
+         *
+         * @return void
+         */
+        public function run ()
+        {
+            foreach ( $this->roles() as $role ) {
+                Role::create(
+                    [
+                        "name" => $role,
+                    ]
+                );
+            }
+        }
+
+        private function roles ()
+        : array
+        {
+            return [
+                "Super Admin",
+                "Content Writer",
+            ];
+        }
+    }

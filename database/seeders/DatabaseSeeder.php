198b55130f68ef35efa3a428bc7a7971e287aeb1
Adrian Leong <leongadrian36@gmail.com>
2022-08-05 23:11:53 +0800

    database seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
     namespace Database\Seeders;
 
     // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+    use App\Models\User;
     use Illuminate\Database\Seeder;
 
     class DatabaseSeeder extends Seeder
@@ -16,10 +17,17 @@
         {
             // \App\Models\User::factory(10)->create();
 
-            \App\Models\User::factory()->create([
-                                                    'name'  => 'Test User',
-                                                    'email' => 'test@example.com',
-                                                ]);
+            User::factory()->create([
+                                        'name'     => 'Admin',
+                                        'email'    => 'admin@admin.com',
+                                        'password' => bcrypt('password'),
+                                    ]);
+
+            User::factory()->create([
+                                        'name'     => 'Editor',
+                                        'email'    => 'editor@editor.com',
+                                        'password' => bcrypt('password'),
+                                    ]);
 
             $this->call(RolesSeeder::class);
         }
--- database/seeders/RolesSeeder.php
+++ database/seeders/RolesSeeder.php
@@ -2,7 +2,9 @@
 
     namespace Database\Seeders;
 
+    use App\Models\User;
     use Illuminate\Database\Seeder;
+    use Spatie\Permission\Models\Permission;
     use Spatie\Permission\Models\Role;
 
     class RolesSeeder extends Seeder
@@ -14,21 +16,18 @@
          */
         public function run ()
         {
-            foreach ( $this->roles() as $role ) {
-                Role::create(
-                    [
-                        "name" => $role,
-                    ]
-                );
-            }
+            $superAdmin = Role::create([ 'name' => 'Super Admin' ]);
+
+            User::find(1)->assignRole($superAdmin);
+
+            $editor = Role::create([ 'name' => 'Editor' ]);
+
+            User::find(2)->assignRole($editor);
+
+            Permission::create([ 'name' => 'view users' ]);
+            Permission::create([ 'name' => 'create users' ]);
+            Permission::create([ 'name' => 'edit users' ]);
+            Permission::create([ 'name' => 'delete users' ]);
         }
 
-        private function roles ()
-        : array
-        {
-            return [
-                "Super Admin",
-                "Content Writer",
-            ];
-        }
     }

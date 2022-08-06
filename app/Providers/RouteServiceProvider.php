eb92508eeecfd914ab8b2ba2a5ffa36ae8b968eb
Adrian Leong <leongadrian36@gmail.com>
2022-08-06 12:27:46 +0800

    move from web to route middleware

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -1,57 +1,60 @@
 <?php
 
-namespace App\Providers;
+    namespace App\Providers;
 
-use Illuminate\Cache\RateLimiting\Limit;
-use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\RateLimiter;
-use Illuminate\Support\Facades\Route;
+    use Illuminate\Cache\RateLimiting\Limit;
+    use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+    use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\RateLimiter;
+    use Illuminate\Support\Facades\Route;
 
-class RouteServiceProvider extends ServiceProvider
-{
-    /**
-     * The path to the "home" route for your application.
-     *
-     * Typically, users are redirected here after authentication.
-     *
-     * @var string
-     */
-    public const HOME = '/dashboard';
-
-    /**
-     * Define your route model bindings, pattern filters, and other route configuration.
-     *
-     * @return void
-     */
-    public function boot()
+    class RouteServiceProvider extends ServiceProvider
     {
-        $this->configureRateLimiting();
+        /**
+         * The path to the "home" route for your application.
+         *
+         * Typically, users are redirected here after authentication.
+         *
+         * @var string
+         */
+        public const HOME = '/dashboard';
 
-        $this->routes(function () {
-            Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('routes/api.php'));
+        /**
+         * Define your route model bindings, pattern filters, and other route configuration.
+         *
+         * @return void
+         */
+        public function boot ()
+        {
+            $this->configureRateLimiting();
 
-            Route::middleware('web')
-                ->group(base_path('routes/web.php'));
+            $this->routes(function () {
+                Route::middleware('api')
+                     ->prefix('api')
+                     ->group(base_path('routes/api.php'))
+                ;
 
-            Route::middleware('web')
-                ->name('admin.')
-                ->prefix('admin')
-                ->group(base_path('routes/admin.php'));
-        });
+                Route::middleware('web')
+                     ->group(base_path('routes/web.php'))
+                ;
+
+                Route::middleware([ 'web', 'inertia' ])
+                     ->name('admin.')
+                     ->prefix('admin')
+                     ->group(base_path('routes/admin.php'))
+                ;
+            });
+        }
+
+        /**
+         * Configure the rate limiters for the application.
+         *
+         * @return void
+         */
+        protected function configureRateLimiting ()
+        {
+            RateLimiter::for('api', function ( Request $request ) {
+                return Limit::perMinute(60)->by($request->user()?->id ? : $request->ip());
+            });
+        }
     }
-
-    /**
-     * Configure the rate limiters for the application.
-     *
-     * @return void
-     */
-    protected function configureRateLimiting()
-    {
-        RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
-        });
-    }
-}

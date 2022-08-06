c4c046e73f39c8139556fda903d8fb610fe8fc90
Adrian Leong <leongadrian36@gmail.com>
2022-08-06 12:48:22 +0800

    fixed next alert message not coming out when first alert is missing

--- resources/js/admin/Components/Alert.vue
+++ resources/js/admin/Components/Alert.vue
@@ -1,19 +1,26 @@
 <script setup>
-import { onMounted, ref, watch } from 'vue';
+import { ref, watch } from 'vue';
 import { usePage } from "@inertiajs/inertia-vue3";
 
-const message = ref(usePage().props.value?.flash?.success)
+const message = ref("")
+const timeoutHandler = ref(null)
 
 watch(() => usePage().props.value?.flash?.success,
-    (successMessage) => message.value = successMessage, {
-        immediate: true
-    })
+    (successMessage) => {
+        message.value = successMessage
 
-onMounted(() => {
-    setTimeout(() => {
-        message.value = ""
-    }, 5000)
-});
+        if (successMessage) {
+            clearTimeout(timeoutHandler.value)
+            timeoutHandler.value = setTimeout(() => {
+                message.value = null
+                usePage().props.value.flash.success = null
+            }, 3000)
+        }
+    }, {
+        immediate: true
+    }
+)
+
 </script>
 <template>
     <div v-if="message" class="bg-green-600 text-white rounded fixed right-0 top-0 px-4 py-2 mr-4 mt-4 z-10">

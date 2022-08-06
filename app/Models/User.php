0896f1202a5f9c256fd6eef0690600f7791505a5
Adrian Leong <leongadrian36@gmail.com>
2022-08-06 20:49:55 +0800

    password cast

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
     namespace App\Models;
 
     // use Illuminate\Contracts\Auth\MustVerifyEmail;
+    use App\Casts\PasswordCast;
     use Illuminate\Database\Eloquent\Factories\HasFactory;
     use Illuminate\Foundation\Auth\User as Authenticatable;
     use Illuminate\Notifications\Notifiable;
@@ -12,18 +13,6 @@
     class User extends Authenticatable
     {
         use HasApiTokens, HasFactory, Notifiable, HasRoles;
-
-        /**
-         * The attributes that are mass assignable.
-         *
-         * @var array<int, string>
-         */
-        protected $fillable
-            = [
-                'name',
-                'email',
-                'password',
-            ];
 
         /**
          * The attributes that should be hidden for serialization.
@@ -44,5 +33,6 @@
         protected $casts
             = [
                 'email_verified_at' => 'datetime',
+                'password'          => PasswordCast::class,
             ];
     }

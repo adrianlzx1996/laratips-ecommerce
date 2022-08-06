c0b67bedf873b5617ffb919cd1b5b45c77ab75dd
Adrian Leong <leongadrian36@gmail.com>
2022-08-06 12:26:41 +0800

    super admin permission

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -1,30 +1,33 @@
 <?php
 
-namespace App\Providers;
+    namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
-use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+    use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+    use Illuminate\Support\Facades\Gate;
 
-class AuthServiceProvider extends ServiceProvider
-{
-    /**
-     * The model to policy mappings for the application.
-     *
-     * @var array<class-string, class-string>
-     */
-    protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
-    ];
-
-    /**
-     * Register any authentication / authorization services.
-     *
-     * @return void
-     */
-    public function boot()
+    class AuthServiceProvider extends ServiceProvider
     {
-        $this->registerPolicies();
+        /**
+         * The model to policy mappings for the application.
+         *
+         * @var array<class-string, class-string>
+         */
+        protected $policies
+            = [
+                // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+            ];
 
-        //
+        /**
+         * Register any authentication / authorization services.
+         *
+         * @return void
+         */
+        public function boot ()
+        {
+            $this->registerPolicies();
+
+            Gate::before(function ( $user, $ability ) {
+                return $user->hasRole('Super Admin') ? true : null;
+            });
+        }
     }
-}

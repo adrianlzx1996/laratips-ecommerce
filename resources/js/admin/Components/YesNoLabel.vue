3a992ef419310a3be34f9ff873afd0c5eeafbb7c
Adrian Leong <leongadrian36@gmail.com>
2022-08-07 16:37:34 +0800

    yesnoLabel component

---
+++ resources/js/admin/Components/YesNoLabel.vue
@@ -0,0 +1,23 @@
+<script setup>
+defineProps({
+    active: Boolean
+})
+</script>
+<template>
+    <Button :color="active ? 'green' : 'red'" small>
+        {{ active ? 'Yes' : 'No' }}
+    </Button>
+</template>
+
+<script>
+import Button from "./Button.vue";
+
+export default {
+    name: "YesNoLabel",
+    components: {Button}
+}
+</script>
+
+<style scoped>
+
+</style>
